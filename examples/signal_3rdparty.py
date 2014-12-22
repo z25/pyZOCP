ca2bfcba18497c0876b65bb7f6058f8c1cbccf32
fieldOfView <aldo@fieldofview.com>
2014-12-22 22:16:18 +0100

    Fixed issue in 3rd party subscriber node

--- examples/signal_3rdparty.py
+++ examples/signal_3rdparty.py
@@ -23,8 +23,11 @@ class SubscriptionMakerNode(ZOCP):
         split_name = name.split("@",1)
         if split_name[0] == 'subscribee':
             self.subscribee_peer = peer
+            self.update_subscription()
+
         elif split_name[0] == 'subscriber':
             self.subscriber_peer = peer
+            self.update_subscription()
 
     def on_modified(self, peer, name, data, *args, **kwargs):
         if self._running and peer:
@@ -32,11 +35,9 @@ class SubscriptionMakerNode(ZOCP):
                 if 'value' in data[key]:
                     self.receive_value(key)
 
-
     def on_peer_signaled(self, peer, name, data, *args, **kwargs):
         if self._running and peer:
             self.receive_value(data[0])
-
 
     def receive_value(self, key):
         new_value = self.capability[key]['value']
@@ -44,11 +45,14 @@ class SubscriptionMakerNode(ZOCP):
         if key == "Subscribe 'My String'":
             if new_value != self.subscribe:
                 self.subscribe = new_value
+                self.update_subscription()
 
-                if self.subscribe:
-                    self.signal_subscribe(self.subscribee_peer, "My String", self.subscriber_peer, "My String")
-                else:
-                    self.signal_unsubscribe(self.subscribee_peer, "My String", self.subscriber_peer, "My String")
+    def update_subscription(self):
+        if self.subscribee_peer is not None and self.subscriber_peer is not None:
+            if self.subscribe:
+                self.signal_subscribe(self.subscribee_peer, "My String", self.subscriber_peer, "My String")
+            else:
+                self.signal_unsubscribe(self.subscribee_peer, "My String", self.subscriber_peer, "My String")
 
         
 if __name__ == '__main__':
